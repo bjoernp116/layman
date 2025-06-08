8915252171f8e0249d28727cd9d1829f45b17f9b
Bjørn Pollen <bjoernpollen@gmail.com>
2025-06-09 01:50:32 +0200

    init

---
+++ sandbox/sandbox.cpp
@@ -0,0 +1,11 @@
+
+
+
+
+#include <iostream>
+#include "layman.h"
+#include <ostream>
+int main (int argc, char *argv[]) {
+    std::cout << add(3, 4) << std::endl;
+    return 0;
+}
---
+++ src/layman.h
@@ -0,0 +1,9 @@
+
+
+
+int layman_add(int x, int y) {
+    return x + y;
+}
+
+
+
